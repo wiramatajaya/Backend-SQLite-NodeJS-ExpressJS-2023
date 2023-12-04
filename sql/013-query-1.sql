115518a10d51d8f2508f78b08252f224a1a88709
Ej Sobrepeña <sobrepenaej@yahoo.com>
2023-12-04 21:43:40 +0200

    sql/013: Add homework

---
+++ sql/013-query-1.sql
@@ -0,0 +1,12 @@
+-- Author: Ej Sobrepena ej.sobrepena@tuni.fi
+-- Date: 2023-12-04
+-- File: 013-query-1.sql
+
+SELECT ename AS "Name",
+       job AS "Job",
+       hiredate AS "Hiring Date",
+       empno AS "Employee Number"
+FROM emp
+;
+
+-- End of file
