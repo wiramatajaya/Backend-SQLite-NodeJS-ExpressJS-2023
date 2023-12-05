6b757eb9a528b06935e2495e918fe666fda9c0bd
Ej Sobrepeña <sobrepenaej@yahoo.com>
2023-12-05 16:25:54 +0200

    sql/013: Remov aliases for columns homework

--- sql/013-query-1.sql
+++ sql/013-query-1.sql
@@ -2,10 +2,10 @@
 -- Date: 2023-12-04
 -- File: 013-query-1.sql
 
-SELECT ename AS "Name",
-       job AS "Job",
-       hiredate AS "Hiring Date",
-       empno AS "Employee Number"
+SELECT ename,
+       job,
+       hiredate,
+       empno
 FROM emp
 ;
 
