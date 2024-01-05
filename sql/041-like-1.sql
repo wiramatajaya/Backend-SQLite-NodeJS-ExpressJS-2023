54e0a5d0105944c6797c6405f0e7496bc2f79b98
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-05 21:23:13 +0200

    sql/041: Add homework

---
+++ sql/041-like-1.sql
@@ -0,0 +1,14 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-05
+-- File: 041-like-1.sql
+
+SELECT
+    ename
+FROM
+    emp
+WHERE
+    ename LIKE '__a%'
+;
+
+
+-- End of file
