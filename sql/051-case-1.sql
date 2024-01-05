0bb09c934cb2bda8c115ee8beff092e300a52308
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-05 21:31:01 +0200

    sql/051: Add homework

---
+++ sql/051-case-1.sql
@@ -0,0 +1,18 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-05
+-- File: 051-case-1.sql
+
+SELECT
+    ename "Name",
+    CASE
+        WHEN job = 'SALESMAN' THEN 'SALES PERSON'
+        ELSE job
+    END "New Job Name"
+FROM
+    emp
+ORDER BY
+    "New Job Name",
+    "Name"
+;
+
+-- End of file
