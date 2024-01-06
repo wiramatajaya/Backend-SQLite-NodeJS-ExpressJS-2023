47ced80805a99896f3f88c2378acd6ca11e51061
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-06 15:33:42 +0200

    sql/082: Add homework

---
+++ sql/082-group-functions-2.sql
@@ -0,0 +1,13 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-06
+-- File: 082-group-functions-2.sql
+
+SELECT
+    COUNT(*) "count of dept 30"
+FROM
+    emp
+WHERE
+    deptno = 30
+;
+
+-- End of file
