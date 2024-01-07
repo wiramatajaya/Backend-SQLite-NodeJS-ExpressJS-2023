c5b8eaa0454c601519d4a187b762ca0d5a922ea5
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-07 12:29:47 +0200

    sql/123: Add homework

---
+++ sql/123-insert-3.sql
@@ -0,0 +1,12 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-07
+-- File: 123-insert-3.sql
+
+INSERT INTO weather (id, time_of_reading, high, low, sig, comment) VALUES
+(1, '2000-12-11 06:00', 5.5, 1.5, 'MK', 'Snow-rain for a while'),
+(2, '2000-12-12 06:00', 4.1, 0.3, 'ES', 'Strong wind'),
+(3, '2000-12-14 06:00', 2.0, -5.8, 'RD', 'Night was cold, foggy weather'),
+(4, '2000-12-15 06:00', 12.8, 7.5, 'MK', 'Rainy day')
+;
+
+-- End of file
