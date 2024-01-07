9a5daae9bc1123b742500426b9b975115271e1a1
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-07 12:25:08 +0200

    sql/112: Add homework

---
+++ sql/112-create-table-2.sql
@@ -0,0 +1,14 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-07
+-- File: 112-create-table-2.sql
+
+CREATE TABLE football (
+    id INT PRIMARY KEY,
+    first VARCHAR(255),
+    last VARCHAR(255),
+    team VARCHAR(255),
+    comment VARCHAR(255)
+)
+;
+
+-- End of file
