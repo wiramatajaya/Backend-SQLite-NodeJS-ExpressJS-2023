dc15cd58d5c24945e204efe1c37be800ead4f9f9
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-07 12:13:40 +0200

    sql/095: Add homework

---
+++ sql/095-multiple-tables-5.sql
@@ -0,0 +1,20 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-07
+-- File: 095-multiple-tables-5.sql
+
+SELECT
+    e.ename,
+    e.job,
+    e.deptno,
+    d.dname
+FROM
+    emp e
+JOIN
+    dept d ON e.deptno = d.deptno
+WHERE
+    (LOWER(d.dname) = 'accounting' OR LOWER(d.dname) = 'sales' OR LOWER(d.dname) = 'operations') AND e.sal > 1000
+ORDER BY
+    e.ename
+;
+
+-- End of file
