25298080f61e48960477a47266fa1a8bc47e4b82
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-07 12:24:59 +0200

    sql/111: Add homework

---
+++ sql/111-create-table-1.sql
@@ -0,0 +1,16 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-07
+-- File: 111-create-table-1.sql
+
+CREATE TABLE person (
+    id INT PRIMARY KEY,
+    last VARCHAR(255),
+    first VARCHAR(255),
+    phone VARCHAR(255),
+    zip VARCHAR(255),
+    city VARCHAR(255),
+    address VARCHAR(255)
+)
+;
+
+-- End of file
