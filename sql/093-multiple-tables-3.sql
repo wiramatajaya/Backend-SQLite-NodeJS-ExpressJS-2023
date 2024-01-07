c1645510671001c3a1038e9d9d7bd721e0282e53
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-07 12:11:03 +0200

    sql/092: Add homework

---
+++ sql/093-multiple-tables-3.sql
@@ -0,0 +1,19 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-07
+-- File: 093-multiple-tables-3.sql
+
+SELECT
+    e.ename,
+    d.dname,
+    d.loc
+FROM
+    emp e
+JOIN
+    dept d ON e.deptno = d.deptno
+WHERE
+    e.comm IS NOT NULL
+ORDER BY
+    e.ename
+;
+
+-- End of file
