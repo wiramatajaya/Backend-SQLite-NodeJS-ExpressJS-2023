a53756e06d4994e73edfb9d6da623dc9714ead51
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-07 12:29:55 +0200

    sql/124: Add homework

---
+++ sql/124-insert-4.sql
@@ -0,0 +1,11 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-07
+-- File: 124-insert-4.sql
+
+INSERT INTO inventory (id, item, worth, comment) VALUES
+(1, 'baseball', 150, 'My first baseball'),
+(2, 'bike', 2000, 'For mountain riding'),
+(3, 'playcards', 10, 'Funny pictures')
+;
+
+-- End of file
