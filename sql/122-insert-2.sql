8f088574978fb1446cfbae23e8e0429b50e05dc3
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-07 12:29:37 +0200

    sql/122: Add homework

---
+++ sql/122-insert-2.sql
@@ -0,0 +1,11 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-07
+-- File: 122-insert-2.sql
+
+INSERT INTO football (id, first, last, team, comment) VALUES
+(1, 'Jim', 'Anders', 'Manchester', 'UPS 10'),
+(2, 'Mark', 'Knofles', 'Liverpool', 'Rockwell international'),
+(3, 'Jenny', 'Richards', 'Highland', 'Scottish national')
+;
+
+-- End of file
