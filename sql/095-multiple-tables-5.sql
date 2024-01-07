f9eec7e53a3b52e548eda9fe1e4f85683d5abf3c
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-07 12:37:48 +0200

    sql/095: Add homework

--- sql/095-multiple-tables-5.sql
+++ sql/095-multiple-tables-5.sql
@@ -8,9 +8,9 @@ SELECT
     e.deptno,
     d.dname
 FROM
-    emp e
+    emp AS e
 JOIN
-    dept d ON e.deptno = d.deptno
+    dept AS d ON e.deptno = d.deptno
 WHERE
     (LOWER(d.dname) = 'accounting' OR LOWER(d.dname) = 'sales' OR LOWER(d.dname) = 'operations') AND e.sal > 1000
 ORDER BY
