7acd9d02a113974f800a3aa2f19e27a785b6dbfd
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-07 12:29:27 +0200

    sql/121: Add homework

---
+++ sql/121-insert-1.sql
@@ -0,0 +1,11 @@
+-- Author: Ej Sobrepena <ej.sobrepena@tuni.fi>
+-- Date: 2024-01-07
+-- File: 121-insert-1.sql
+
+INSERT INTO person (id, last, first, phone, zip, city, address) VALUES
+(1, 'Doe', 'John', '123455', '11122', 'New York', '2th avenue'),
+(2, 'Jordan', 'Mike', '221122', '01212', 'Washington', 'South Park 3'),
+(3, 'Durak', 'Stephen', '550011', '55654', 'Miami', 'Sea Drive 112')
+;
+
+-- End of file
