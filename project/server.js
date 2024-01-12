e77d0f1dc112946f37a2434a2ce2882a26b3886b
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-12 03:21:21 +0200

    project/server: Set up SQLite database connection

--- project/server.js
+++ project/server.js
@@ -6,3 +6,12 @@ const port = 8000;
 const HTTP_STATUS_BAD_REQUEST = 400;
 const HTTP_STATUS_NOT_FOUND = 404;
 const DB_PATH = './database.db';
+
+// Open database in memory
+let db = new sqlite3.Database(DB_PATH, sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+        console.error(err.message);
+        process.exit(1); // Exit if database connection fails
+    }
+    console.log('Connected to the SQlite database.');
+});
