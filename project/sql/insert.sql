a4352bb804b52f2aa0e9b1ed4b1192890f43eab8
Ej Sobrepeña <sobrepenaej@yahoo.com>
2024-01-12 02:21:13 +0200

    project/insert: Add sql to insert sample data into the Users table

---
+++ project/sql/insert.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO Users (first, last, dept) VALUES ('John', 'Doe', 10);
+INSERT INTO Users (first, last, dept) VALUES ('Jane', 'Doe', 20);
+INSERT INTO Users (first, last, dept) VALUES ('Jim', 'Doe', 30);
